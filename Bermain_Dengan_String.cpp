3b25fdf05ccd47424d5ecfdcb41cb2085f57083b
muhammadIdhamMaarif <muhammadidhammaarif@gmail.com>
2024-09-27 07:37:56 +0700

    Add files via upload

---
+++ Bermain_Dengan_String.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nama1, nama2;
+    cin >> nama1;
+    cin >> nama2;
+    cout << nama1 << " " << nama2;
+    return 0;
+}
